01b2afe61a61779385e8ca74811485fc984aaa70
Justin <binning.justin@gmail.com>
2025-08-10 19:02:42 -0700

    removed static from def

--- executor.h
+++ executor.h
@@ -4,10 +4,10 @@
 #define EXECUTOR_H
 
 int run_command(char **args);
-static int search_path_alloc(const char *cmd, char **outp);
-static bool has_slash(const char *s); 
-static bool is_directory(const char *path);
-static bool is_regular(const char *path);
-static bool is_executable(const char *path);
+int search_path_alloc(const char *cmd, char **outp);
+bool has_slash(const char *s); 
+bool is_directory(const char *path);
+bool is_regular(const char *path);
+bool is_executable(const char *path);
 
 #endif  // EXECUTOR_H
