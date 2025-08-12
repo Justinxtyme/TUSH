6f4010a8b2adf5586d04d6aca4553a4f7392743c
Justin <binning.justin@gmail.com>
2025-08-11 23:13:50 -0700

    ..

--- signals.c
+++ signals.c
@@ -11,7 +11,7 @@
 
 
 /* Called once in main() before REPL to ignore SIGINT/SIGQUIT in the shell */
-static void setup_parent_signals(void) {
+void setup_parent_signals(void) {
     struct sigaction sa = {0};
     sa.sa_handler = SIG_IGN;
     sigaction(SIGINT,  &sa, NULL);
@@ -19,7 +19,7 @@ static void setup_parent_signals(void) {
 }
 
 /* Called in every child before exec to restore default signal behavior */
-static void setup_child_signals(void) {
+void setup_child_signals(void) {
     struct sigaction sa = {0};
     sa.sa_handler = SIG_DFL;
     sigaction(SIGINT,  &sa, NULL);
