10f11a82cbacb69fb25b8268084174af107df02a
Justin <binning.justin@gmail.com>
2025-08-12 22:41:55 -0700

    ..

--- builtins.h
+++ builtins.h
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 #ifndef BUILTINS_H
 #define BUILTINS_H
-
+#endif
 int handle_cd(char **args);
 int handle_exit(char **args);
-bool is_builtin(const char *cmd);
+bool is_builtin(const char *cmd)
