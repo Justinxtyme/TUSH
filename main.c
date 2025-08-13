221d0ddbb53ef9b5c4030168bcda9b445b5b16be
Justin <binning.justin@gmail.com>
2025-08-12 22:09:04 -0700

    ..

--- main.c
+++ main.c
@@ -54,7 +54,7 @@ int main() {
             perror("expand_variables");
             continue;
         }
-        LOG(LOG_LEVEL_DEBUG, "Expanded input: '%s'", expanded);
+        LOG(LOG_LEVEL_INFO, "Expanded input: '%s'", expanded);
 
         char ***cmds = parse_pipeline(expanded, &num_cmds);
         free(expanded);
